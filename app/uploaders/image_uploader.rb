cf2853eb21597528af64f51229336e75e7012175
yoshiaki ohashi <ohashi.spdc@icloud.com>
2021-06-11 17:21:03 +0900

    画像のファイル名をランダムに変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -47,4 +47,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   def size_range
     0..5.megabytes
   end
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
